673a5c21f24ac4d7a7c79d618dc0dd1101d16776
chrisParkinson <chrisgparkinson@gmail.com>
2020-11-01 14:53:14 +0000

    Sets up basic project structure
    Creates a model based on munro csv file

---
+++ model/src/main/java/parkinson/chris/model/Munro.java
@@ -0,0 +1,267 @@
+package java.parkinson.chris.model;
+
+public class Munro {
+
+    private Integer runningNo;
+    private Integer doBIHNumber;
+    private String streetMapUrl;
+    private String geographUrl;
+    private String hillBaggingUrl;
+    private String name;
+    private Integer smcSection;
+    private String rhbSection;
+    private String section;
+    private String heightMeters;
+    private String heightFeet;
+    private String mapOneFifty;
+    private String mapOneTwentyFive;
+    private String gridRef;
+    private String gridRefXY;
+    private String xcoord;
+    private String ycoord;
+    private MunroClassification classification1891;
+    private MunroClassification classification1921;
+    private MunroClassification classification1933;
+    private MunroClassification classification1953;
+    private MunroClassification classification1969;
+    private MunroClassification classification1974;
+    private MunroClassification classification1981;
+    private MunroClassification classification1984;
+    private MunroClassification classification1990;
+    private MunroClassification classification1997;
+    private MunroClassification classificationPost1997;
+    private String comments;
+
+    public Integer getRunningNo() {
+        return runningNo;
+    }
+
+    public void setRunningNo(Integer runningNo) {
+        this.runningNo = runningNo;
+    }
+
+    public Integer getDoBIHNumber() {
+        return doBIHNumber;
+    }
+
+    public void setDoBIHNumber(Integer doBIHNumber) {
+        this.doBIHNumber = doBIHNumber;
+    }
+
+    public String getStreetMapUrl() {
+        return streetMapUrl;
+    }
+
+    public void setStreetMapUrl(String streetMapUrl) {
+        this.streetMapUrl = streetMapUrl;
+    }
+
+    public String getGeographUrl() {
+        return geographUrl;
+    }
+
+    public void setGeographUrl(String geographUrl) {
+        this.geographUrl = geographUrl;
+    }
+
+    public String getHillBaggingUrl() {
+        return hillBaggingUrl;
+    }
+
+    public void setHillBaggingUrl(String hillBaggingUrl) {
+        this.hillBaggingUrl = hillBaggingUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSmcSection() {
+        return smcSection;
+    }
+
+    public void setSmcSection(Integer smcSection) {
+        this.smcSection = smcSection;
+    }
+
+    public String getRhbSection() {
+        return rhbSection;
+    }
+
+    public void setRhbSection(String rhbSection) {
+        this.rhbSection = rhbSection;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getHeightMeters() {
+        return heightMeters;
+    }
+
+    public void setHeightMeters(String heightMeters) {
+        this.heightMeters = heightMeters;
+    }
+
+    public String getHeightFeet() {
+        return heightFeet;
+    }
+
+    public void setHeightFeet(String heightFeet) {
+        this.heightFeet = heightFeet;
+    }
+
+    public String getMapOneFifty() {
+        return mapOneFifty;
+    }
+
+    public void setMapOneFifty(String mapOneFifty) {
+        this.mapOneFifty = mapOneFifty;
+    }
+
+    public String getMapOneTwentyFive() {
+        return mapOneTwentyFive;
+    }
+
+    public void setMapOneTwentyFive(String mapOneTwentyFive) {
+        this.mapOneTwentyFive = mapOneTwentyFive;
+    }
+
+    public String getGridRef() {
+        return gridRef;
+    }
+
+    public void setGridRef(String gridRef) {
+        this.gridRef = gridRef;
+    }
+
+    public String getGridRefXY() {
+        return gridRefXY;
+    }
+
+    public void setGridRefXY(String gridRefXY) {
+        this.gridRefXY = gridRefXY;
+    }
+
+    public String getXcoord() {
+        return xcoord;
+    }
+
+    public void setXcoord(String xcoord) {
+        this.xcoord = xcoord;
+    }
+
+    public String getYcoord() {
+        return ycoord;
+    }
+
+    public void setYcoord(String ycoord) {
+        this.ycoord = ycoord;
+    }
+
+    public MunroClassification getClassification1891() {
+        return classification1891;
+    }
+
+    public void setClassification1891(MunroClassification classification1891) {
+        this.classification1891 = classification1891;
+    }
+
+    public MunroClassification getClassification1921() {
+        return classification1921;
+    }
+
+    public void setClassification1921(MunroClassification classification1921) {
+        this.classification1921 = classification1921;
+    }
+
+    public MunroClassification getClassification1933() {
+        return classification1933;
+    }
+
+    public void setClassification1933(MunroClassification classification1933) {
+        this.classification1933 = classification1933;
+    }
+
+    public MunroClassification getClassification1953() {
+        return classification1953;
+    }
+
+    public void setClassification1953(MunroClassification classification1953) {
+        this.classification1953 = classification1953;
+    }
+
+    public MunroClassification getClassification1969() {
+        return classification1969;
+    }
+
+    public void setClassification1969(MunroClassification classification1969) {
+        this.classification1969 = classification1969;
+    }
+
+    public MunroClassification getClassification1974() {
+        return classification1974;
+    }
+
+    public void setClassification1974(MunroClassification classification1974) {
+        this.classification1974 = classification1974;
+    }
+
+    public MunroClassification getClassification1981() {
+        return classification1981;
+    }
+
+    public void setClassification1981(MunroClassification classification1981) {
+        this.classification1981 = classification1981;
+    }
+
+    public MunroClassification getClassification1984() {
+        return classification1984;
+    }
+
+    public void setClassification1984(MunroClassification classification1984) {
+        this.classification1984 = classification1984;
+    }
+
+    public MunroClassification getClassification1990() {
+        return classification1990;
+    }
+
+    public void setClassification1990(MunroClassification classification1990) {
+        this.classification1990 = classification1990;
+    }
+
+    public MunroClassification getClassification1997() {
+        return classification1997;
+    }
+
+    public void setClassification1997(MunroClassification classification1997) {
+        this.classification1997 = classification1997;
+    }
+
+    public MunroClassification getClassificationPost1997() {
+        return classificationPost1997;
+    }
+
+    public void setClassificationPost1997(MunroClassification classificationPost1997) {
+        this.classificationPost1997 = classificationPost1997;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+}
---
+++ model/src/main/java/parkinson/chris/model/MunroClassification.java
@@ -0,0 +1,6 @@
+package java.parkinson.chris.model;
+
+public enum MunroClassification {
+    MUN,
+    TOP;
+}
